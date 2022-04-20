e297eb48aa175b344eecd613f35ae1b5dd4b452f
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2022-04-20 13:53:54 -0300

    adição de funções

--- operadores.php
+++ operadores.php
@@ -9,5 +9,9 @@ abs() a funcao retorna o valor absoluto(positivo) de um numero
 sqrt() a funcao retorna a raiz quadrada de um numero
 round() a funcao arredona um numero de ponto flutuante para o inteiro mais proximo (ex: 1.4 = 1|| 1.6 = 2)
 rand() a funcao gera um numero aleatorio
+* pow() Potenciação
+* intval() Pega a parte inteira do número
+* number_format($variavel,casas decimais) Formata o número - semelhante ao {:.2f} em python
+* number_format($var, decimal, ",") caso queira que o número seja separado por virgula adicionar esse ultimo campo
 
 */
