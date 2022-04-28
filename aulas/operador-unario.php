f11b43861d729651d6ca813d35ad9b102bf5f83d
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2022-04-28 14:18:30 -0300

    aulas

---
+++ aulas/operador-unario.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Expressão ? verdadeiro : falso
+$a > $b ? $a : $b
+$maior = $a > $b ? $a : $b "Se $a for > que $b, o $maior recebera $a, se $a < $b, o $maior recebera $b"
+
+Exemplo usando algoritmo
+
+SE (a > b) entao
+    maior <-a
+SENAO
+    maior <-b
+FIMSE
+
+*/
---
+++ aulas/operadores-relacionais.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Maior $a > $b
+Menor $a < $b
+Maior ou Igual $a >= $b
+Menor ou Igual $a <= $b
+Diferente $a <> $b ou $a != $b
+Igual $a == $b
+Identico $a === $b
+
+*/
