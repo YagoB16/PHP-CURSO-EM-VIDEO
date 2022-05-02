5431c6abfd958147f17181a79b800d4b758a73e9
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2022-05-02 18:49:32 -0300

    aulas

--- aulas/operadores-incremento.php
+++ aulas/operadores-incremento.php
@@ -5,7 +5,7 @@ sendo necessário usar web para adicionar valor á variável
 */
 
     
-    $atual = $_GET["aa"]; // Essa linha vai pegar o ano na URL
+    $atual = $_GET["aa"]; // O $_GET nessa linha vai pegar o valor de "aa" na URL
     echo "O ano atual é $atual" . PHP_EOL ;
     echo "O ano anterior é" . --$atual . PHP_EOL; 
     $atual++;// Usado para retornar o valor do ano para o que está na URL
