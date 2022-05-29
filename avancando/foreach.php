6606523727f40c9d6bdf178aba2a0174c4232865
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2022-05-29 15:02:55 -0300

    ajustes nome do arquivo

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -16,7 +16,8 @@ $contasCorrentes = [
         'saldo' => 1500
     ]
 ];
-
+//Para acessar um item
+echo $contasCorrentes[12345678890]['saldo'] . PHP_EOL;
 
 foreach($contasCorrentes as $cpf => $conta){
     echo $cpf . PHP_EOL;
