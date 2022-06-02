99317f40ce88e018f583ba5f4e18ec575d5a7f82
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2022-06-02 14:27:35 -0300

    adicionar contas sem passar valor entre []

--- avancando/adicao-contas.php
+++ avancando/adicao-contas.php
@@ -1,22 +1,17 @@
 <?php
 /**
  * O foreach é ideal para percorrer listas de dados (como arrays).
- * Se tiver um array associativo onde as chaves tem algum significado, sempre que for adicionar um novo item é preciso adicionar a chave que no caso seria o cpf entre ['aqui']
- * $contasCorrentes ['198.768.987-32'] = [
-    'titular' => 'Dhiogo',
-    'saldo' => 100
-];
  */
 $contasCorrentes = [
-    '123.456.788-90' => [
+    12345678890 => [
         'titular' => 'Yago',
         'saldo' => 1000
     ],
-    '193.347.689-01' => [
+    19334768901 => [
         'titular' => 'Samuel',
         'saldo'=> 1200
     ],
-    '172.661.234-45' => [
+    17266123445 => [
         'titular' => 'Caio',
         'saldo' => 1500
     ]
@@ -25,26 +20,29 @@ $contasCorrentes = [
 /*$contasCorrentes [123123432] = [
     'titular' => 'Dhiogo',
     'saldo' => 100
-]; //sera adicionado o item com os valores presentes aqui que no caso são "titular" com o valor "dhiogo" e "saldo" com o valor "100" e seu cpf será o que esta entre[]*/
+]; //sera adicionado os valores presentes aqui que no caso são "titular" com o valor "dhiogo" e "saldo" com o valor "100" e seu cpf será o que esta entre[]*/
 
 $contasCorrentes [] = [
     'titular' => 'Dhiogo',
     'saldo' => 100
-]; //sera adicionado com o indice numerico 0, porque no esse será o primeiro indice numerico, ja que os que estão sendo passados anteriormente são string
-
-
-$contasCorrentes [87266123445] = [
-    'titular' => 'Dhiogo2',
-    'saldo' => 1000
-]; // aqui é outro exemplo onde é colocado um valor numerico dentro dos []
+]; //sera adicionado com o indice do maior numero dentre os demais gerados anteriormente  e acrescentado + 1 no ultimo.
+//por exemplo o maior numero é 87266123445 (cpf do caio) o do cpf do dhiogo será (87266123446), mudando o final 45 para 46
 
 $contasCorrentes [] = [
-    'titular' => 'Dhiogo3',
+    'titular' => 'Dhiogo2',
     'saldo' => 1000
-]; // aqui o valor será 87266123445 + 1, pois o ultimo valor é o cadastrado acima Dhiogo02
+]; // aqui é outro exemplo que sera com o final 47
 
 foreach($contasCorrentes as $cpf => $conta){  //o valor de $contasCorrentes é atribuido a $cpf 
     echo $cpf . " " . $conta['titular'] . PHP_EOL;
 }
 
-/* Aqui vemos que o valor do cpf é uma string, sendo assim, eles não terão nenhum indice numerico, por isso os dois ultimos cadastrados estão como 0 e 1, pois serão os primeiros valores INT */
+/*O valor dos cpf são passados como um INT, não teremos problemas para gerar um indice no [] em branco
+$contasCorrentes [] = [
+    'titular' => 'Dhiogo2',
+    'saldo' => 1000
+];
+
+Porém essa situação muda caso seja uma STRING, veremos no arquivo conta2.php
+
+*/
