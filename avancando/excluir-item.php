75576d96011b0bd4651f3607d6825780ac6b5dfe
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2022-06-09 14:53:44 -0300

    Create excluir-item.php

---
+++ avancando/excluir-item.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'funcoes.php';
+
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.456.789-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.456.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 1000
+    ]
+];
+
+
+$contasCorrentes['123.456.789-10'] = deposito(
+    $contasCorrentes['123.456.789-10'],
+    valorDeposito: 500
+);
+$contasCorrentes['123.456.789-11'] = deposito(
+    $contasCorrentes['123.456.789-11'], 
+    valorDeposito: 500
+);
+$contasCorrentes['123.456.789-12'] = sacar(
+    $contasCorrentes['123.456.789-12'],
+    valorASacar: 500
+);
+
+unset($contasCorrentes['123.456.789-11']); //unset é usado para excluir algum item da lista
+
+titularComNomeMaiusculas($contasCorrentes['123.456.789-10']);
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    list('titular' => $titular, 'saldo' => $saldo) = $conta; // ou  ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    exibeMensagem(
+        mensagem: "$cpf $titular $saldo"
+    );
+}
