e86687567d8b8817440e0274a1ec188c28085d04
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2022-06-19 21:33:02 -0300

    ajuste

--- orientação/class.php
+++ orientação/class.php
@@ -8,15 +8,15 @@ class conta // classe seria o modelo utilizado para criar um objeto
 }
 
 
-$primeiraConta = new conta();
+$primeiraConta = new conta();// aqui criaremos uma nova conta $primeiraConta
 
-$primeiraConta -> saldo = 200;
+$primeiraConta -> saldo = 200; // adicionamos saldo nessa primeira conta atraves do -> e =
 
-$primeiraConta -> cpfTitular = '123.456.789-10';
+$primeiraConta -> cpfTitular = '123.456.789-10';// adicionamos cpfTitular
 
-$primeiraConta -> nomeTitular = 'Yago';
+$primeiraConta -> nomeTitular = 'Yago';// adicionamos nomeTitular
 
-$segundaConta = new conta();
+$segundaConta = new conta();// aqui criaremos outra conta $segundaConta e o ciclo se repete
 
 $segundaConta -> saldo = 2000;
 
@@ -26,4 +26,4 @@ $segundaConta -> cpfTitular = '123.456.789-11';
 
 var_dump($primeiraConta, $segundaConta);
 
-echo $segundaConta -> saldo; // para acessar uma informação utilizar ->
+echo $segundaConta -> saldo; // para acessar uma informação utilizar 'local onde está a informação'-> 'informação que quer buscar'
